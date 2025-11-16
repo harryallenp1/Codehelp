21eed77a015da0c10822ea0dcc7a1a4704d2f22d
kumateja <kumateja@sheridancollege.ca>
2025-11-16 18:54:49 -0500

    Create sample function for DataUtils that leverages DataService

--- ApplicationLayer/LogicalOperations/DataUtils.py
+++ ApplicationLayer/LogicalOperations/DataUtils.py
@@ -1,3 +1,27 @@
 #By: Ed Wang 
 #Manipulating Data 
 
+# from ApplicationLayer.LogicalOperations import OptionsUtils
+from ApplicationLayer.DataServiceAPI import DataService
+import pandas as pd 
+import asyncio
+
+
+
+async def Get_Program_University_Data(ProgramID):
+    try:
+        df = await DataService.RequestLatestKPIForProgram(ProgramID=ProgramID)
+        return df
+    
+
+    except Exception as e:
+        print(f"Get_Program_University_Data(ProgramID={ProgramID})=>{e}")
+        return pd.DataFrame()
+
+
+#Testing -> Open CMD and navigate to Education-Planning-Dashboard, then run python -m ApplicationLayer.LogicalOperations.DataUtils
+
+df= asyncio.run(Get_Program_University_Data(ProgramID=3))
+
+
+print(df)
