76b3add5ba3fd7405611601388ce9a7f20888204
Ed Wang <wanged@sheridancollege.ca>
2025-11-17 17:03:19 -0500

    DataUtils in application layer

--- ApplicationLayer/LogicalOperations/DataUtils.py
+++ ApplicationLayer/LogicalOperations/DataUtils.py
@@ -29,6 +29,32 @@ def Get_Program_University_Data(ProgramID):
         print(f"Get_Program_University_Data(ProgramID={ProgramID})=>{e}")
         return pd.DataFrame()
 
+def Get_Program_Occupation_Data(ProgramID):
+    try:
+        df = DataService.RequestProgramNOCLinks(ProgramID=ProgramID)
+        
+        return df
+    except Exception as e:
+        print(f"Get_Program_Occupation_Data(ProgramID={ProgramID})=>{e}")
+        return pd.DataFrame()
 
+def Get_NOC_Historical_Data(NOC_GroupingID,DGUID):
+    try:
+        df = DataService.RequestNOCGroupLaborStatistics(NOC_GroupingID=NOC_GroupingID,DGUID=DGUID)
 
+        return df
+
+    except Exception as e:
+        print(f"Get_NOC_Historical_Data(NOC_GroupingId={NOC_GroupingID},DGUID={DGUID}) => {e}")
+        return pd.DataFrame()
+    
+def Get_Latest_ER_NOC_PCT(NOC_GroupingID, provinceID):
+    try:
+        df = DataService.RequestEconomicRegionEmploymentEstimate(NOC_GroupingID=NOC_GroupingID, provinceID=provinceID)
+
+        return df
+
+    except Exception as e:
+        print(f"Get_Latest_ER_NOC_PCT(NOC_GroupingId={NOC_GroupingID},provinceID={provinceID}) => {e}")
+        return pd.DataFrame()
 
