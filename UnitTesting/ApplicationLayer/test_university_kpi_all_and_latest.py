e8def5ed479582b131c288eee004182c045b6731
Ed Wang <wanged@sheridancollege.ca>
2025-12-07 23:27:06 -0500

    bug fix

--- UnitTesting/ApplicationLayer/test_university_kpi_all_and_latest.py
+++ UnitTesting/ApplicationLayer/test_university_kpi_all_and_latest.py
@@ -34,8 +34,8 @@ with open(RESULT_FILE, "w", encoding="utf-8") as f:
 
 #region -------- SUCCESS TESTS -------- #
 EXPECTED_SUCCESS_ENDPOINTS = [
-    (University_KPI_History_Check, (3,'2020 Employment Rate 2 Years after Graduation')),
-    (University_KPI_History_Check, (4,'2020 Employment Rate 6 Months after Graduation')),
+    (University_KPI_History_Check, (3,'Employment Rate 2 Years after Graduation')),
+    (University_KPI_History_Check, (4,'Employment Rate 6 Months after Graduation')),
     (Latest_KPI_Program_data, (3,)),
     (Latest_KPI_Program_data, (4, ))
 
